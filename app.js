bfbb2dd271e51926cf61384b5f3048e34f23efb0
saumya <2saumyaray@gmail.com>
2017-06-01 14:51:50 +0530

    wip, the website

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var products = require('./routes/products');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -43,6 +45,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/products', products);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
