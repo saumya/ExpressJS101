1131ecbbf31353c0f2843993a0a8645859885fed
saumya <2saumyaray@gmail.com>
2017-06-06 13:06:03 +0530

    product details

--- routes/products.js
+++ routes/products.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('products', { title: 'Hand Made Products' });
 });
 
+router.get('/1', function(req, res, next) {
+  res.render('productDetail', { title: 'Product 1' });
+});
+
 module.exports = router;
